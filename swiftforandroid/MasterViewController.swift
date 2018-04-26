dfbcb08dfe8b30b0f33ebe0f89b78fe7c1946b4e
Darius Bell <dcb13a@mbb-302-mac-02.local>
2018-04-25 20:02:10 -0500

    tried

--- swiftforandroid/MasterViewController.swift
+++ swiftforandroid/MasterViewController.swift
@@ -11,23 +11,52 @@ import UIKit
 class MasterViewController: UITableViewController {
 
     var detailViewController: DetailViewController? = nil
-    var dat = DataController()
+    let TURL = "https://api.myjson.com/bins/1ahrbf"
+    var franchises = [String]()
+    var objectsReturned = [DataObject]()
+    var myfulldata = [[DataObject]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        dat.getJsonFromUrl()
         let imageView = UIImageView(image:#imageLiteral(resourceName: "Image"))
         self.navigationItem.titleView = imageView
         self.navigationController?.navigationBar.barTintColor = UIColor.darkGray
-//        // Do any additional setup after loading the view, typically from a nib.
-//        navigationItem.leftBarButtonItem = editButtonItem
-//
-//        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-//        navigationItem.rightBarButtonItem = addButton
-//        if let split = splitViewController {
-//            let controllers = split.viewControllers
-//            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-//        }
+        getJsonFromUrl()
+    }
+    
+    func getJsonFromUrl(){
+        //creating a NSURL
+        let url = NSURL(string: TURL)
+        
+        //fetching the data from the url
+        URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
+            
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                
+                if let allDatData = jsonObj!.value(forKey: "datData") as? NSDictionary{
+                    print(allDatData)
+                    if let franArrary = allDatData.value(forKey: "franchise") as? NSArray {
+                        for franchiseDict in franArrary {
+                            if let fName = (franchiseDict as! NSDictionary).value(forKey: "fName") as? String{
+                                self.franchises.append(fName)
+                                
+                                if let entriesArray = (franchiseDict as! NSDictionary).value(forKey: "entries") as? NSArray {
+                                    self.objectsReturned = []
+                                    for entryobj in entriesArray {
+                                        let thisEn = DataObject(withObject: entryobj as! Dictionary<String, AnyObject>)
+                                        print(thisEn.name)
+                                        self.objectsReturned.append(thisEn)
+                                    }
+                                }
+                            }
+                            self.myfulldata.append(self.objectsReturned)
+                        }
+                    }
+                    print(self.myfulldata)
+                }
+                
+            }
+        }).resume()
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -64,15 +93,21 @@ class MasterViewController: UITableViewController {
     // MARK: - Table View
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return self.franchises.count
     }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.myfulldata[section].count
+    }
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return self.franchises[section]
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let obj =  myfulldata[indexPath.section][indexPath.row]
+        cell.textLabel?.text = obj.name
+        cell.detailTextLabel?.text = obj.yearStart
         return cell
     }
 
